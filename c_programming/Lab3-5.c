b47f0a839ca043218fd84f463df222fcb2636be2
banzkung <s7070007@kmitl.ac.th>
2015-01-23 11:30:51 +0700

    DE_ref1130_L25Wk

--- c_programming/Lab3-5.c
+++ c_programming/Lab3-5.c
@@ -3,16 +3,17 @@
 int main(void)
 {
     // define neccesary variables
-    int null_in_centimetres;
-    float null_cen_to_metres, radius;
+    float null_in_centimetres, radius, volume, surface;
 
     // input data to program
-    scanf("%d", &null_in_centimetres);
+    scanf("%f", &null_in_centimetres);
 
     // calculate
-    radius = null_in_centimetres/2;
+    radius = null_in_centimetres/2.0;
+    volume = (4 * 3.1416 * pow(radius, 3))/3.0;
+    surface = 4.0 * 3.1416 * pow(radius, 2);
 
     // print output
-    printf("Volume: %f m^3\n", ((4 * 3.1416 * pow(radius, 3))/3)/1000000.0);
-    printf("Surface Area: %f m^2\n", (4 * 3.1416 * pow(radius, 2))/10000.0);
+    printf("Volume: %.4f m^3\n", volume/1000000.0);
+    printf("Surface Area: %.4f m^2\n", surface/10000.0);
 }
