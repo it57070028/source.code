2d170555397fa3f4d0a5756e682da2d2a139a6a4
banzkung <s7070007@kmitl.ac.th>
2015-01-23 12:31:07 +0700

    DE_ref1203_L35W3_FN

--- c_programming/Lab3-5.c
+++ c_programming/Lab3-5.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-#include <math.h>
+#define pi 3.1416
 int main(void)
 {
     // define neccesary variables
-    float null_in_centimetres, radius, volume, surface;
+    float null_in_centimetres, radius;
 
     // input data to program
     scanf("%f", &null_in_centimetres);
 
     // calculate
     radius = null_in_centimetres/2.0;
-    volume = (4.0 * 3.14160 * pow(radius, 3.0))/3.0;
-    surface = 4.0 * 3.14160 * pow(radius, 2.0);
 
     // print output
-    printf("Volume: %.4f m^3\n", volume/1000000.0);
-    printf("Surface Area: %.4f m^2\n", surface/10000.0);
+    printf("Volume: %.4f m^3\n", (4.0/3.0) * pi * (radius * radius * radius)/1000000.0);
+    printf("Surface Area: %.4f m^2\n", 4.0 * pi * (radius * radius)/10000.0);
 }
